011e6e44c26a88e22eb2553c88810c0bc8cc7ff0
z-e-a <e.a.zubkov@yandex.ru>
2025-02-03 01:30:29 +0300

    feat: add error boundary

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,14 @@
 import { StrictMode } from 'react';
 import { Container, createRoot } from 'react-dom/client';
 import './index.css';
-import App from './App.tsx';
+import App from './app/App.tsx';
+import ErrorBoundary from './app/ErrorBoundary.tsx';
 
 const root: Container = document.getElementById('root') ?? document.body;
 createRoot(root).render(
   <StrictMode>
-    <App />
+    <ErrorBoundary>
+      <App />
+    </ErrorBoundary>
   </StrictMode>
 );
