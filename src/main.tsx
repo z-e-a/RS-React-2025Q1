e09c2b26eb89bc4a4a489ee6850c767f3596e47c
z-e-a <e.a.zubkov@yandex.ru>
2025-02-10 02:46:43 +0300

    feat: add router

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,45 @@
-import { StrictMode } from 'react';
-import { Container, createRoot } from 'react-dom/client';
-import './index.scss';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 import App from './app/App.tsx';
+import './index.scss';
 import ErrorBoundary from './app/ErrorBoundary.tsx';
+import {
+  createBrowserRouter,
+  Navigate,
+  RouterProvider,
+} from 'react-router-dom';
+import NotFound from './pages/NotFound';
+import PersonDetail from './widgets/PersonDetail';
 
-const root: Container = document.getElementById('root') ?? document.body;
-createRoot(root).render(
-  <StrictMode>
-    <ErrorBoundary>
-      <App />
-    </ErrorBoundary>
-  </StrictMode>
+export const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Navigate to="search" replace />,
+    errorElement: <NotFound />,
+  },
+  {
+    path: 'search/*',
+    element: (
+      <ErrorBoundary>
+        <App />
+      </ErrorBoundary>
+    ),
+    errorElement: <NotFound />,
+    children: [
+      {
+        path: 'detail',
+        element: <PersonDetail />,
+      },
+    ],
+  },
+  {
+    path: '*',
+    element: <NotFound />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root') ?? document.body).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
 );
